4d6e205138c5a47cc76c9bbfd9ad45fbe0dc32a7
Grant Tepper <grant.tepper@gmail.com>
2014-04-03 23:46:43 -0400

    updating general file with syntax

--- php/general.php
+++ php/general.php
@@ -7,6 +7,102 @@
  * This is a docblock comment 
  */
 
+// Arithmetic Operations
+
+$a = 2;
+$b = 8;
+echo '$a = 2, $b = 8';
+
+// Negation
+echo 'Negation: -$a'."\n";
+echo -$a."\n\n";
+
+
+// Addition
+echo 'Addition: $a+$b'."\n";
+echo $a+$b."\n\n";
+
+// Subtraction
+echo 'Subtraction: $a-$b'."\n";
+echo $a-$b."\n\n";
+
+// Multiplication
+echo 'Multiplication: $a*$b'."\n";
+echo $a*$b."\n\n";
+
+// Division
+echo 'Division: $a/$b'."\n";
+echo $a/$b."\n\n";
+
+// Modulus
+echo 'Modulus : $b % $a'."\n";
+echo $b%$a."\n";
+
+echo 'Modulus : $a % $b'."\n";
+echo $a%$b;
+
+
+// Concatination - a series of interconnected things or events
+$foo = 'Hello';
+$bar = 'World';
+
+echo "\n".$foo.' '.$bar."\n\n";
+
+// Additional Assignment Operators
+$a += 2;
+$a -= 2;
+$foo .= $bar;
+
+// Interpolation -  to put (something) between other things
+echo "$foo";
+
+
+/* 
+ * Comparison Operators 
+ */
+
+// Equal to
+$a == $b;
+
+// Identical  Equal to AND of the same type
+$a === $b;
+
+// Not Equal
+$a != $b; // use this one 
+$a <> $b;
+
+// Not Identical True if $a != $b OR they are not the same type
+$a !== $b;
+
+// Greater than
+$a > $b;
+ 
+// Less than
+$a < $b;
+
+// Less than or Equal to
+$a <= $b;
+
+// Greater than or equal to
+$a >= $b;
+
+
+/* 
+ * Logical Operators
+ */
+
+// Not -- TRUE if $a is not TRUE
+!$a;
+
+// And -- TRUE if both $a and $b are true 
+$a && $b; 
+
+// Or -- TRUE if $a or $b are true
+$a || $b;
+
+
+
+
 /* 
  * Helpfull functions
  */
