3bcacc78d8a5c137fc1aa607ca2ac98a5d9f922a
Grant Tepper <grant.tepper@gmail.com>
2014-04-04 01:22:56 -0400

    finishing up php templating slides

--- php-templating/phpTemplatingExample1.php
+++ php-templating/phpTemplatingExample1.php
@@ -1,6 +1,31 @@
 <?php
 
-    $myData = array (
+    $myPeople = array (
+        array(
+            'name' => 'Charles Babbage',
+            'fields' => array('mathematics', 'engineering', 'computer science', 'political economy')
+            'contribution' => 'Credited with the conept for the programable computer'
+        ),
+        array(
+            'name' => 'Alan Turing',
+            'fields' => array('mathematics', 'logician', 'computer science', 'cryptanalyst', 'philosopher'),
+            'contribution' => 'Inventor / Father of modern computing'
+        ),
+        array(
+            'name' => 'Miss Augusta Ada King, Countes of Lovelace (Ada Lovelace)',
+            'fields' => array('countess', 'mathematician'),
+            'contribution' => 'The first computer progammer ever. Translated documents to run on Charles Babbages analytical engine, making the first algorithm ever.'
+        ),
+        array(
+            'name' => 'Grace Murray Hopper',
+            'fields' => array('US Navy Rear Admril', 'computer science', ),
+            'contribution' => 'One of the first programers ever.  Wrote the first compiler for a computer language ever. Credited with the popularization of \'debugging\'.'
+        ),
+        array(
+            'name' => 'Jean Bartik',
+            'fields' => array('mathematics', 'computer science'),
+            'contribution' => 'Played a major role in working on the first digital, Turing-Complete computer ever constructed (ENIAC)'
+        )
     );
 ?>
 
@@ -15,9 +40,24 @@
         <div class="row">
             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
             <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                <ul>
-                    <li></li>
-                </ul>
+                <?php if (count($myPeople)): ?>
+                    <table>
+                        <tr>
+                            <th>Index</th>
+                            <th>Name</th>
+                            <th>Fields</th>
+                            <th>Contribution</th>
+                        </tr>
+                        <?php foreach($myPeople as $index => $person): ?>
+                            <tr>
+                                <td><?php echo $index ?></td>
+                                <td><?php echo ucfirst($person['name']) ?></td>
+                                <td><?php foreach($person['fields'] as $f){ echo ucfirst($f).', '; } ?></td>
+                                <td><?php echo $person['contribution'] ?></td>
+                            </tr>
+                        <?php endforeach?>
+                    </table>
+                <?php  endif; ?>
             </div>
             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
         </div>
