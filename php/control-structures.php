9a4b9e324d6743e1c1229d54a1e8efe5487153ba
Grant Tepper <grant.tepper@gmail.com>
2014-04-04 02:12:41 -0400

    updated control structures

--- php/control-structures.php
+++ php/control-structures.php
@@ -37,3 +37,45 @@ if ($a == $b){
 }
 
 // Alternative syntax used in HTML templates go check out php-templating/phpTemplateExmaple1.php
+
+
+// Switch Statments
+
+/*
+ * What if you have like 20 if else blocks? gets messy... 
+ * You could use a switch!
+ */
+
+$num = mt_rand(0, 10);
+
+switch ($num) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        echo '0-4';
+        break;
+}
+
+
+// return - returns sopmething, or nothing from the current scope
+// will also end a script if the next highest scope is standard output
+function someFunction(){
+    return 'Hello World';
+}
+
+echo someFunction();
+
+// include Includes a file into the script, upon failure a warning will be issued
+// this is helpful when pulling in configuration
+$config = include __DIR__.'/files/config.php';
+
+// include_once -- same as include, but will only ever happen once
+
+// require -- same a include but will throw a PHP FATAL error on failure
+
+// this is actually more correct as configuration files are generally REQUIRED for an application to run
+$config = require __DIR__.'/files/config.php';
+
+// require also has a require_once version of itself
